7bbe7c61a48cb127513119e19dd4c0ff3f9a726b
josh619 <vjoshuadan@gmail.com>
2024-03-06 20:23:32 +0530

    editor setup using package

---
+++ src/components/TextEditor.js
@@ -0,0 +1,29 @@
+import React, { Component, useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+const TextEditor = () => {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+  };
+
+  return (
+    <div>
+      <Editor
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        onEditorStateChange={onEditorStateChange}
+      />
+      ;
+    </div>
+  );
+};
+
+export default TextEditor;
